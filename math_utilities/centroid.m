5163cc38f54a071e31391e8bc047373eee022112
n1oftheabove <35739070+n1oftheabove@users.noreply.github.com>
2018-07-04 13:45:40 +0200

    Update centroid.m

--- math_utilities/centroid.m
+++ math_utilities/centroid.m
@@ -1,6 +1,9 @@
 % calculates the centroid of two vectors with customizable index range (start: schwstart, end: schwend)
 
 % think of the vector xxx as a vector consisting of 1 dimensional space coordinates, and yyy as a vector containing the masses corresponding to that coordinates. Then the centrois is calculated as  sum(xxx_i * yyy_i) / sum(yyy_i)
+%
+%
+% created by n1oftheabove, 2013 at Potsdam University. Use as you like
 
 
 function [schw] = schwerpunkt(xxx,yyy,schwstart,schwend)
