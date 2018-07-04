7fda9fbd8f6e4b4db3d00be1bef9ef9681789d2f
n1oftheabove <35739070+n1oftheabove@users.noreply.github.com>
2018-07-04 13:42:40 +0200

    Update and rename slofftanalyzer.m to Fast Fourier Transformation/slofftanalyzer.m

--- Fast Fourier Transformation/slofftanalyzer.m
+++ Fast Fourier Transformation/slofftanalyzer.m
@@ -1,11 +1,11 @@
-%this function demands a set of (non double) reflectivity and aequidistant
-%delayvalues ('reflectivity' and 'delay'), a lower index 'k' and an upper
-%index 'l', cuts the data in that index range, produces a smoothed one with
-%a given 'smoothrange', subtracts the unsmoothed with the smoothed one,
-%applies a window function and outputs the FFT of this ('fftcut' over
-%'fftcut'). Also for the FFT provide information for the pulsewidth
-%('pulsew') remark: Window-function 'window_1.m' necessary. parameter
-%'optionall' = 1, cutdelay and rdy2windowref is given for the whole spectrum
+% this function demands a set of (non double) reflectivity and aequidistant
+% delayvalues ('reflectivity' and 'delay'), a lower index 'k' and an upper
+% index 'l', cuts the data in that index range, produces a smoothed one with
+% a given 'smoothrange', subtracts the unsmoothed with the smoothed one,
+% applies a window function and outputs the FFT of this ('fftcut' over
+% 'fftcut'). Also for the FFT provide information for the pulsewidth
+% ('pulsew') remark: Window-function 'window_1.m' necessary. parameter
+% 'optionall' = 1, cutdelay and rdy2windowref is given for the whole spectrum
 % (incl the area before zero)
 
 function [ fftcutfreq, fftcut, cutdelay, rdy2windowref ] = slofftanalyzer(delay, reflectivity, k, l, smoothrange, pulsew, optionall)
@@ -28,19 +28,19 @@ else
 end
 samples= length(delay);
 
-T = delayrange./samples;      %time intervall between two measured points
-Fs = 1/T;       % Sampling Frequency in THz
+T = delayrange./samples;                                        % time intervall between two measured points
+Fs = 1/T;                                                       % Sampling Frequency in THz
       
 NFFT = length(cutdelay);
 Y = fft(windwd_corcut, NFFT)/NFFT;
 f = Fs/2 * linspace(0,1,floor(NFFT/2)+1);
 Ampdens = 2*abs(Y(1:floor(NFFT/2)+1))';
-%erster Index in frequenzvektor, ab dem Frequenz gr��er als Pulsbreite
-%klein ist
+
+% first index in frequency vector, from which on frequency is bigger than inverse pulse width
+
 fmax = find(f >(1000*1./pulsew),1,'first');
 fftcutfreq= f(1:fmax);
 fftcut= Ampdens(1:fmax);
-
 
 if optionall == 1
     cutrefall = zeros(l,1);
@@ -49,4 +49,3 @@ if optionall == 1
     rdy2windowref = cutrefall;
     cutdelay = delay(1:l);
 end
-
